bbbd6b3d24a7b720bb2ae8a65f320d89279c7b66
inseang0133-svg <inseang0133@gmail.com>
2025-12-13 21:24:59 +0700

    Add files via upload

---
+++ web.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+def run():
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000))
